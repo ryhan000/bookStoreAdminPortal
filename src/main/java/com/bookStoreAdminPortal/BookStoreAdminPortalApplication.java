c4685f9dd61fffc862954929a9309aeb5c9800ab
ryhan000 <rayhan1ahamed@gmail.com>
2018-01-26 20:08:26 +0600

    Added AdminPortal - Book Entity

---
+++ src/main/java/com/bookStoreAdminPortal/BookStoreAdminPortalApplication.java
@@ -0,0 +1,12 @@
+package com.bookStoreAdminPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreAdminPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreAdminPortalApplication.class, args);
+	}
+}
