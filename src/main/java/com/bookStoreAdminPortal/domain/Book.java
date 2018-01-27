f24bcbf95f8cfe8e04b2c36e935f5c779d96d4e2
ryhan000 <rayhan1ahamed@gmail.com>
2018-01-27 12:16:22 +0600

    Added AdminPortal - Home Template

--- src/main/java/com/bookStoreAdminPortal/domain/Book.java
+++ src/main/java/com/bookStoreAdminPortal/domain/Book.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -33,7 +34,7 @@ public class Book {
 	private String description;
 	private int inStockNumber;
 	
-	
+	@Transient
 	private MultipartFile bookImage;
 
 
